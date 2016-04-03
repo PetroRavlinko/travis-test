cbc76f8645456bff694d49ef22c39181b24e2ba2
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-03 12:48:02 -0300

    Uncomment mv command

--- configure-mvn.sh
+++ configure-mvn.sh
@@ -3,7 +3,7 @@
 echo "Changing maven configuration"
 echo "Moving new settings.xml to /usr/local/maven/"
 
-#sudo mv settings.xml $HOME/.m2/settings.xml
+sudo mv settings.xml $HOME/.m2/settings.xml
 
 echo "New Maven configuration DONE"
 
