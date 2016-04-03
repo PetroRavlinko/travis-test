323195f265d7279f4a0cf088be520e01848de880
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-03 12:26:34 -0300

    Try to change mvn settings file

---
+++ configure-mvn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Changing maven configuration"
+echo "Moving new settings.xml to /usr/local/maven/"
+
+mv settings.xml /usr/local/maven/settings.xml
+
+echo "New Maven configuration DONE"
+
