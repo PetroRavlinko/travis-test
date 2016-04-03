ee80f10b43781fc900f30eefa771f8fbd2b11450
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-03 12:50:54 -0300

    Print directories content

--- configure-mvn.sh
+++ configure-mvn.sh
@@ -5,5 +5,11 @@ echo "Moving new settings.xml to /usr/local/maven/"
 
 sudo mv settings.xml $HOME/.m2/settings.xml
 
+ls $HOME/.m2/
+
+echo "Print content of /usr/local/maven/"
+
+ls /usr/local/maven/
+
 echo "New Maven configuration DONE"
 
