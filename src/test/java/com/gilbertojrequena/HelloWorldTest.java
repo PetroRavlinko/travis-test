75e708651394da7668eb6917aaf125fa17216cb9
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-03 13:09:45 -0300

    Add test

---
+++ src/test/java/com/gilbertojrequena/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.gilbertojrequena;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Gilberto J Requena
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void easyTest() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
