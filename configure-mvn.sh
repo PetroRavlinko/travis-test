5d5f8315fcd13b1c6422a971980c907330908374
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-03 12:38:42 -0300

    Change maven conf location

--- configure-mvn.sh
+++ configure-mvn.sh
@@ -3,7 +3,7 @@
 echo "Changing maven configuration"
 echo "Moving new settings.xml to /usr/local/maven/"
 
-sudo mv settings.xml /usr/local/maven/settings.xml
+sudo mv settings.xml $HOME/.m2/settings.xml
 
 echo "New Maven configuration DONE"
 
