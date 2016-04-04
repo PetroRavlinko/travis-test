5bacf205674f1dd51247ae88aad206cf2fce5b3f
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-04 06:44:44 -0300

    Make Release

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 mvn clean
 git checkout master
 snapshotVersion=$(mvn help:evaluate -Dexpression=project.version | tail -8 | head -1)
-version=(${snapshotVersion//-/ })
+IFS='-' eval 'version=($snapshotVersion)'
 mvn -B release:prepare
 mvn release:branch -DbranchName=travis-test-$version[1]
 mvn -B release:perform
