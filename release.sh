c46a8b7248eadc96e4ce45bc8ee8d190ff263613
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-04 07:07:03 -0300

    Make Release

--- release.sh
+++ release.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
 
+version=-1
+
+echo "mvn clean"
 mvn clean
+echo "git checkout master"
 git checkout master
+
+echo "Getting current version"
 snapshotVersion=$(mvn help:evaluate -Dexpression=project.version | tail -8 | head -1)
-IFS='-' eval 'version=($snapshotVersion)'
+echo "The current version is ${snapshotVersion}"
+version=${snapshotVersion%-SNAPSHOT}
+
+echo "mvn -B release:prepare"
 mvn -B release:prepare
-mvn release:branch -DbranchName=travis-test-$version[1]
+echo "mvn release:branch -DbranchName=travis-test-${version}"
+mvn release:branch -DbranchName=travis-test-${version}
+echo "mvn -B release:perform"
 mvn -B release:perform
