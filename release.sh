c7a71244c6ea673801a903a327680da30df268f5
Gilberto J Requena <gilberto_requena@gap.com>
2016-04-04 10:36:02 -0300

    Release v0.0.25

--- release.sh
+++ release.sh
@@ -5,7 +5,27 @@ mvn clean
 echo "git checkout master"
 git checkout master
 
-echo "mvn -B release:prepare"
-mvn -B release:prepare
-echo "mvn -B release:perform"
-mvn -B release:perform
+echo "Getting current version"
+snapshotVersion=$(mvn help:evaluate -Dexpression=project.version | tail -8 | head -1)
+
+echo "The current version is ${snapshotVersion}"
+version=${snapshotVersion%-SNAPSHOT}
+
+echo "Changing project version to the release version v$version"
+
+echo "mvn versions:set -DnewVersion=$version"
+mvn versions:set -DnewVersion=${version}
+rm pom.xml.versionsBackup
+
+echo "Pushing $version branch on github"
+git add .
+git commit -m "Release v${version}"
+git push origin master
+git checkout -b ${version}
+git push origin ${version}
+
+
+#echo "mvn -B release:prepare"
+#mvn -B release:prepare
+#echo "mvn -B release:perform"
+#mvn -B release:perform
