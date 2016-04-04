05941b9d87a2489f02149b4043b699e929563371
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-04 11:28:07 -0300

    Stop using maven release plugin to perform releases

--- release.sh
+++ release.sh
@@ -15,16 +15,31 @@ echo "Changing project version to the release version v$version"
 
 echo "mvn versions:set -DnewVersion=$version"
 mvn versions:set -DnewVersion=${version}
-rm pom.xml.versionsBackup
+mvn versions:commit
 
-echo "Pushing $version branch on github"
-git add .
+echo "Pushing release $version to the master"
+git add pom.xml
 git commit -m "Release v${version}"
 git push origin master
+echo "Pushing $version tag"
+git tag -a v${version} -m "Release of version ${version}"
+git push --tags
+
+echo "Creating tag"
+release_json='{ "tag_name": '"${version}"', "target_commitish": "master", "name": '"${version}"', "body": "Release of version '"${version}"'", "draft": false, "prerelease": false}'
+curl -u "${GIT_USER_ACCOUNT}:${GIT_PASSWORD}" -H "Content-Type: application/json" -X POST -d ${release_json} https://api.github.com/repos/"${GIT_USER_ACCOUNT}"/travis-test/releases
+
+echo "Creating $version branch"
 git checkout -b ${version}
 git push origin ${version}
 
+echo "Updating to the new ${version}-SNAPSHOT version"
+release:update-versions
+git add pom.xml
+git commit -m "Prepare for the next development version"
+git push origin master
 
+#Stop using release plugin
 #echo "mvn -B release:prepare"
 #mvn -B release:prepare
 #echo "mvn -B release:perform"
