41595795f120eef2994ee3de38bd55228bd77e30
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-04 07:17:15 -0300

    Make Release

--- release.sh
+++ release.sh
@@ -7,14 +7,7 @@ mvn clean
 echo "git checkout master"
 git checkout master
 
-echo "Getting current version"
-snapshotVersion=$(mvn help:evaluate -Dexpression=project.version | tail -8 | head -1)
-echo "The current version is ${snapshotVersion}"
-version=${snapshotVersion%-SNAPSHOT}
-
 echo "mvn -B release:prepare"
 mvn -B release:prepare
-echo "mvn release:branch -DbranchName=travis-test-${version}"
-mvn release:branch -DbranchName=travis-test-${version}
 echo "mvn -B release:perform"
 mvn -B release:perform
