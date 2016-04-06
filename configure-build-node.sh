cf908b841b8fc844f0ad929f34dfa1a4086963fa
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-05 22:47:32 -0300

    Make Release

--- configure-build-node.sh
+++ configure-build-node.sh
@@ -21,4 +21,5 @@ echo "New Maven configuration... DONE"
 echo "Configuring git"
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
+git config --global credential.helper cache
 echo "Git configuration... DONE"
