1b453d98abf7a6dfa5e46725efc4624f6c2ca48f
Gilberto J Requena <gilbertojrequena@gmail.com>
2016-04-07 12:38:24 -0300

    Make Release

--- src/main/java/com/gilbertojrequena/HelloWorld.java
+++ src/main/java/com/gilbertojrequena/HelloWorld.java
@@ -3,6 +3,6 @@ package com.gilbertojrequena;
 public class HelloWorld {
 
     public static void main(String[] params) {
-        System.out.println("Hello my great new world!");
+        System.out.println("Hello my great new java world!");
     }
 }
